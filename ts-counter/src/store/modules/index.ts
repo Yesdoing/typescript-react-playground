3fe65e3c8c911f887867b247e51ba9d841f91402
Jane Lee <Yesdoing13@gmail.com>
2019-02-22 00:14:35 +0900

    remove two project

--- ts-counter/src/store/modules/index.ts
+++ ts-counter/src/store/modules/index.ts
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux';
 import counter, { CounterState } from './counter';
+import todos, { TodosState } from './todos';
+
 
 export default combineReducers<CounterState>({
-  counter
+  counter,
+  todos
 } as any);
 
 // 스토어의 상태 타입 정의
 export interface StoreState {
   counter: CounterState;
+  todos: TodosState;
 }
